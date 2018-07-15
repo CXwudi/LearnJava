79d66bab359c864dd7ef98fc4855183bd2bee751
CX无敌 <CX@192.168.56.1>
2018-07-15 00:40:00 -0400

    change module

--- src/module-info.java
+++ src/module-info.java
@@ -2,11 +2,11 @@
  * 
  */
 /**
- * @author CXwudi
+ * @author CX无敌
  *
  */
-module Practice {
-	requires java.base; //All programs need this
+module LearnJava {
+	exports newjava8910feature;
 	requires java.logging;
-	requires javafx.base; //JavaFx needs this
+	requires javafx.base;
 }
