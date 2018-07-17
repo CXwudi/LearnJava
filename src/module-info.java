561f8f3936e96a2674a876715ae368c678863d2e
CX无敌 <CX@192.168.56.1>
2018-07-16 23:24:52 -0400

    Just notice you don't need to create it

--- src/module-info.java
+++ src/module-info.java
@@ -3,7 +3,7 @@
  */
 /**
  * @author CX无敌
- *
+ * You don't need to create the module by yourself, you can use Eclipse's right-click project menu to configure 
  */
 module LearnJava {
 	exports newjava8910feature;
