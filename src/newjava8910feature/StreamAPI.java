2ec2124b6c0f624f9f0b2daf9dd625df6b8d48d1
CXwudi <1113421658@qq.com>
2018-08-11 00:43:48 -0400

    Try some StramAPI practice

--- src/newjava8910feature/StreamAPI.java
+++ src/newjava8910feature/StreamAPI.java
@@ -1,8 +1,16 @@
 package newjava8910feature;
 
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.function.IntPredicate;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamAPI {
 
@@ -10,12 +18,22 @@ public class StreamAPI {
 		return x > 1 && IntStream.range(2, x).map(i -> x % i).noneMatch(i -> i == 0);
 	}
 	
-	public static Set<Integer> getDivisors(int x){
-		return null;//TODO: Can you use stream api to do an one line code
+	public static List<Integer> getDivisors(int x){
+		return IntStream.rangeClosed(1, x)
+				.filter(i -> x % i ==0)
+				.mapToObj(Integer::valueOf).sorted()
+				.collect(Collectors.toList());
+	}
+	
+	public static List<Integer> getCommonDivisor(int x, int y) {
+		return x > y ? getCommonDivisor(y, x) : IntStream.rangeClosed(1, x).filter(i -> x % i == 0 && y % i == 0).mapToObj(Integer::valueOf).collect(Collectors.toList());
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(checkPrime(79));
+		System.out.println("79 is " + (checkPrime(79) ? "prime" : "not prime"));
+		new Thread(() -> System.out.println(getDivisors(34578636))).start();
+		new Thread(() -> System.out.println(getDivisors(210983748))).start();
+		System.out.println(getCommonDivisor(34578636,210983748));
 	}
 	
 }
