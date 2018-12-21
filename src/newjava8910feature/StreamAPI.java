4026db91f6171c1b0c0b466bee546e240e89ae01
CXwudi <1113421658@qq.com>
2018-12-20 19:13:41 -0500

    something

--- src/newjava8910feature/StreamAPI.java
+++ src/newjava8910feature/StreamAPI.java
@@ -1,16 +1,11 @@
 package newjava8910feature;
 
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.function.IntPredicate;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class StreamAPI {
 
@@ -22,7 +17,7 @@ public class StreamAPI {
 		return IntStream.rangeClosed(1, x)
 				.parallel()
 				.filter(i -> x % i ==0)
-				.mapToObj(Integer::valueOf)
+				.mapToObj(Integer::valueOf) // same as .boxed()
 				.sorted()
 				.collect(Collectors.toList());
 	}
@@ -32,8 +27,32 @@ public class StreamAPI {
 			IntStream.rangeClosed(1, x)
 			.parallel()
 			.filter(i -> x % i == 0 && y % i == 0)
-			.mapToObj(Integer::valueOf)
+			.boxed()// .mapToObj(Integer::valueOf) // are the same
 			.collect(Collectors.toList());
+	}
+	
+	public static void testCollect(int r) {
+		ArrayList list = IntStream.rangeClosed(0, r).parallel()
+				.collect(ArrayList<Integer>::new, (l,n) -> l.add(n), (l1, l2)->l1.addAll(l2));
+		ArrayList list2 = IntStream.rangeClosed(0, r).parallel()
+				.collect(ArrayList<Integer>::new, ArrayList<Integer>::add, ArrayList<Integer>::addAll);
+		System.out.println(list);
+		System.out.println(list2);
+	}
+	
+	public static void testSort(ArrayList<Integer> list) {
+		var l1 = list.parallelStream();
+		var l2 = new ArrayList<>(list);
+		var start = System.nanoTime();
+		System.out.println("start = " + start);
+		l1.sorted();// I dont know how to test stream sort 
+		System.out.println("end = " + (System.nanoTime() - start));
+		start = System.nanoTime();
+		System.out.println("start = " + start);
+		l2.sort(Comparator.naturalOrder());
+		System.out.println("end = " + (System.nanoTime() - start));
+		
+		
 	}
 	
 	public static void main(String[] args) {
@@ -41,6 +60,10 @@ public class StreamAPI {
 		new Thread(() -> System.out.println(getDivisors(34578636))).start();
 		new Thread(() -> System.out.println(getDivisors(210983748))).start();
 		System.out.println(getCommonDivisor(34578636,210983748));
+		testCollect(10);
+		var testList = IntStream.range(0, 10000000).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toCollection(ArrayList::new));
+		//System.out.println(testList);
+		testSort(testList);
 	}
 	
 }
