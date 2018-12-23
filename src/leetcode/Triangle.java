aeaa6fce63b02197426a6dc4610506a2f5dacadb
CXwudi <1113421658@qq.com>
2018-12-23 14:46:42 -0500

    add comment

--- src/leetcode/Triangle.java
+++ src/leetcode/Triangle.java
@@ -12,16 +12,15 @@ public class Triangle {
 		int[] results = new int[triangle.get(triangle.size() - 1).size() + 1]; 
 		for (int i = triangle.size() - 1; i >= 0; i--) {
 			List<Integer> levelI = triangle.get(i);
-			//int[] currentResult = new int[levelI.size()];
 			for (int j = 0; j < levelI.size(); j++) {
+				//you can overwrite the results because in this for loop, no loop back on i-1.
 				results[j] = levelI.get(j) + Math.min(results[j], results[j+1]);
 			}
-			//results = currentResult;
 		}
 		return results[0];
+		
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
