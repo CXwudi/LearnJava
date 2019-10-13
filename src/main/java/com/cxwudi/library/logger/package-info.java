402363dfd972f288466710eeeb2ac8407c3e4013
CXwudi <1113421658@qq.com>
2019-10-12 21:29:56 -0700

    add sample usage to package info

--- src/main/java/com/cxwudi/library/logger/package-info.java
+++ src/main/java/com/cxwudi/library/logger/package-info.java
@@ -1,5 +1,6 @@
 /**
- * my useful logger pre-setup so that people is happy with the log message
+ * my useful logger pre-setup so that people is happy with the log message.
+ * A sample usage can be that {@code var logger = MyPredefinedLogger.setupInvincibleLoggerFor("My Miku Logger");}
  * @author CX无敌
  */
 package com.cxwudi.library.logger;
