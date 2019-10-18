0450c1b56b8e7cdb52583d51ec39fac4fa0207ae
CXwudi <1113421658@qq.com>
2019-10-18 00:21:40 -0700

    successfully made a working logback.xml file

--- src/main/java/com/cxwudi/playground/SFL4JLogger.java
+++ src/main/java/com/cxwudi/playground/SFL4JLogger.java
@@ -1,13 +1,14 @@
 package com.cxwudi.playground;
 
 import java.lang.invoke.MethodHandles;
-
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SFL4JLogger {
+	public static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	public static void main(String[] args) {
-		var logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-		logger.info("Hi Miku");
+		LOGGER.info("Hi Miku");
+		LOGGER.warn("Hi Luka");
 	}
 }
