f2fcf6131e7282ff1adabbfa850ec01e56f89fbc
CXwudi <1113421658@qq.com>
2019-10-19 12:00:56 -0700

    move logback.xml file, able to customize logback.xml file location

--- src/main/java/com/cxwudi/playground/SFL4JLogger.java
+++ src/main/java/com/cxwudi/playground/SFL4JLogger.java
@@ -1,14 +1,19 @@
 package com.cxwudi.playground;
 
+import java.io.File;
 import java.lang.invoke.MethodHandles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.util.ContextInitializer;
+
 public class SFL4JLogger {
-	public static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	public static void main(String[] args) {
+		System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, new File("src/main/java/com/cxwudi/playground/logback.xml").toString());
+		Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 		LOGGER.info("Hi Miku");
 		LOGGER.warn("Hi Luka");
+		LOGGER.debug(new File("src/main/java/com/cxwudi/playground/logback.xml").getAbsolutePath());
 	}
 }
