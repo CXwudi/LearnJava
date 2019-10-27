d17fcdb15cf9ed6376a99353c8d1da291de5006b
CXwudi <1113421658@qq.com>
2019-10-26 22:10:12 -0700

    exclude any logback code and use log4j2 instead

--- src/main/java/com/cxwudi/playground/SFL4JLogger.java
+++ src/main/java/com/cxwudi/playground/SFL4JLogger.java
@@ -5,15 +5,15 @@ import java.lang.invoke.MethodHandles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.util.ContextInitializer;
+//import ch.qos.logback.classic.util.ContextInitializer;
 
 public class SFL4JLogger {
-
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	public static void main(String[] args) {
-		System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, new File("src/main/java/com/cxwudi/playground/logback.xml").toString());
-		Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-		LOGGER.info("Hi Miku");
-		LOGGER.warn("Hi Luka");
-		LOGGER.debug(new File("src/main/java/com/cxwudi/playground/logback.xml").getAbsolutePath());
+		//System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, new File("src/main/java/com/cxwudi/playground/logback.xml").toString());
+		logger.info("Hi Miku");
+		logger.warn("Hi Luka");
+		logger.error("Oh no");
+		logger.debug(new File("src/main/java/com/cxwudi/playground/logback.xml").getAbsolutePath());
 	}
 }
