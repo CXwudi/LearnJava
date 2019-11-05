44ffd1bdffb70b3388444224da9f40a9ad63f58d
CXwudi <1113421658@qq.com>
2019-11-04 22:47:23 -0800

    add a LazyVar cool class

---
+++ src/main/java/com/cxwudi/library/util/LazyVar.java
@@ -0,0 +1,48 @@
+package com.cxwudi.library.util;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * A wrapper class of any class instance to achieve lazy evaluation, 
+ * which allow such instance been not created only until {@link LazyVar#get()} is called. 
+ * And such instance is stored for next {@link LazyVar#get()} function call to avoid re-creation
+ * @author CX无敌
+ *
+ * @param <T> an instance to be created lazily
+ */
+public class LazyVar<T> {
+	/**
+	 * the variable to be stored
+	 */
+	private Optional<T> objectOpt;
+	
+	/**
+	 * the create function 
+	 */
+	private Supplier<T> createFunction;
+	
+	public LazyVar (Supplier<T> createInstanceFunc){
+		objectOpt = Optional.empty();
+		createFunction = createInstanceFunc;
+	}
+
+	/** 
+	 * get or create the object using {@link LazyVar#createFunction}
+	 * @return the object
+	 */
+	public T get() {
+		if (objectOpt.isEmpty()) {
+			objectOpt = Optional.of(createFunction.get());
+		} 
+		return objectOpt.get();
+	}
+
+	/**
+	 * set the object, do so will replace {@link LazyVar#createFunction} with this object
+	 * @param object the object to set
+	 */
+	public void set(T object) {
+		objectOpt = Optional.of(object);
+	}
+}
