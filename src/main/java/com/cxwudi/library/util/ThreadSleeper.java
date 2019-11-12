c38432dd36b18f2cce6f8481775d0fc238b2ba06
CXwudi <1113421658@qq.com>
2019-11-11 23:22:02 -0800

    Add a class that allows calling Thread.sleep() without try catch

--- src/main/java/com/cxwudi/library/util/ThreadSleeper.java
+++ src/main/java/com/cxwudi/library/util/ThreadSleeper.java
@@ -1,5 +1,11 @@
 package com.cxwudi.library.util;
 
+/**
+ * A class that allows calling Thread.sleep() without writing try catch statement in order to achieve better code readability,
+ * @warn don't use it in multi-thread program
+ * @author CX无敌
+ *
+ */
 public interface ThreadSleeper {
 	
 	public static boolean sleep(long millis) {
